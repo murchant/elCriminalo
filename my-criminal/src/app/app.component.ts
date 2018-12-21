a2a18aabc1b5ae3cef23b3c20632435baba36f25
murchant <mamurtag@tcd.ie>
2018-12-21 13:38:31 +0000

    date fix

--- my-criminal/src/app/app.component.ts
+++ my-criminal/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-criminal';
+  date1 = new Date(2018, 7, 3);
+  date2 = new Date(2017, 6, 14);
+  date3 = new Date(2017, 7, 11);
+  date4 = new Date(2015, 7, 1);
 }
